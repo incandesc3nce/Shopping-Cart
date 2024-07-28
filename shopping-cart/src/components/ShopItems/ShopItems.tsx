fb328b29d4e94f82212d92a4940a688406643a2e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 14:59:58 +0700

    feat: create ShopItems component

---
+++ shopping-cart/src/components/ShopItems/ShopItems.tsx
@@ -0,0 +1,17 @@
+import items from './items';
+import ItemCard from '../ItemCard/ItemCard';
+
+const ShopItems = () => (
+  <section className="m-5">
+    <h2 className="mt-4 text-center text-3xl font-semibold">Shop Items</h2>
+    <div
+      className="mt-4 grid gap-4"
+    >
+      {items.map((item) => (
+        <ItemCard key={item.id} item={item} />
+      ))}
+    </div>
+  </section>
+);
+
+export default ShopItems;
