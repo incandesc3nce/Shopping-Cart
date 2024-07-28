47c571a06aa8d29d5d53b93b37d4fd0142751523
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 11:51:06 +0700

    feat: improve semantics

--- shopping-cart/src/components/NavBar/Navbar.tsx
+++ shopping-cart/src/components/NavBar/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => (
-  <div className="pl-4 pr-4 flex space-x-10 p-2 font-sans bg-sky-200">
+  <header className="pl-4 pr-4 flex space-x-10 p-2 font-sans bg-sky-200">
     <h1 className="text-3xl font-bold">KUWS</h1>
     <nav className="flex w-full items-center">
       <ul className="flex w-full list-none space-x-4">
@@ -8,7 +8,7 @@ const Navbar = () => (
       </ul>
       <div>Cart</div>
     </nav>
-  </div>
+  </header>
 );
 
 export default Navbar;
