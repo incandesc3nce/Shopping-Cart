129601520f1f4401c363806ef989006a379fad5f
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 14:17:20 +0700

    feat: create item objects array

---
+++ shopping-cart/src/components/ShopItems/items.ts
@@ -0,0 +1,66 @@
+import IItemCard from '../../interfaces/IItemCard';
+
+const items: IItemCard[] = [
+  {
+    id: 1,
+    title: 'White Kaws Figure',
+    price: 100,
+    imgUrl: 'src/assets/kaws-items/item_1.png',
+  },
+  {
+    id: 2,
+    title: 'Black Kaws Figure',
+    price: 200,
+    imgUrl: 'src/assets/kaws-items/item_2.png',
+  },
+  {
+    id: 3,
+    title: 'Split Kaws Figure',
+    price: 300,
+    imgUrl: 'src/assets/kaws-items/item_3.png',
+  },
+  {
+    id: 4,
+    title: 'Supreme Kaws Figure',
+    price: 400,
+    imgUrl: 'src/assets/kaws-items/item_4.png',
+  },
+  {
+    id: 5,
+    title: 'Stormtrooper Kaws Figure',
+    price: 500,
+    imgUrl: 'src/assets/kaws-items/item_5.png',
+  },
+  {
+    id: 6,
+    title: 'Money Kaws Figure',
+    price: 600,
+    imgUrl: 'src/assets/kaws-items/item_6.png',
+  },
+  {
+    id: 7,
+    title: 'Kaws Black T-Shirt',
+    price: 700,
+    imgUrl: 'src/assets/kaws-items/item_7.png',
+  },
+  {
+    id: 8,
+    title: 'Kaws White T-Shirt',
+    price: 800,
+    imgUrl: 'src/assets/kaws-items/item_8.png',
+  },
+  {
+    id: 9,
+    title: 'Batman Kaws Figure',
+    price: 900,
+    imgUrl: 'src/assets/kaws-items/item_9.png',
+  },
+  {
+    id: 10,
+    title: 'Kaws Chum Figure',
+    price: 1000,
+    imgUrl: 'src/assets/kaws-items/item_10.png',
+  },
+];
+
+export default items;
