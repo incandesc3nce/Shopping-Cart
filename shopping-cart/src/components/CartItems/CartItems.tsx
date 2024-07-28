a7de83218b43b7c3d1275b52db7da85a25906c20
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 18:40:07 +0700

    feat: show cart items

--- shopping-cart/src/components/CartItems/CartItems.tsx
+++ shopping-cart/src/components/CartItems/CartItems.tsx
@@ -1,5 +1,60 @@
-const CartItems = () => (
-  <div>Cart Items</div>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setArray,
+  addToArray,
+  removeFromArray,
+  clearArray,
+} from '../../store/arraySlice';
+
+const CartItems = () => {
+  const array = useSelector((state: any) => state.array);
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearArray());
+  };
+
+  const handleRemoveItem = (item: any) => {
+    dispatch(removeFromArray(item));
+  };
+
+  return (
+    <section>
+      <h2 className="mt-4 text-center text-3xl font-semibold">Cart Items</h2>
+      <div className="m-2 flex flex-col gap-4">
+        {array.map((arrayItem: any) => (
+          <article className="min-h-30 flex items-center justify-between rounded-3xl bg-slate-200 p-1 shadow-md">
+            <img
+              src={arrayItem.item.imgUrl}
+              alt={arrayItem.item.title}
+              className="h-20 w-20 rounded-full object-cover"
+            />
+            <h3 className="max-w-24 text-center text-xl font-semibold">
+              {arrayItem.item.title}
+            </h3>
+            <div>
+              <p className="text-xl">
+                ${arrayItem.item.price * arrayItem.quantity}
+              </p>
+              <p>Quantity: {arrayItem.quantity}</p>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="button"
+                onClick={() => {
+                  handleRemoveItem(arrayItem);
+                }}
+                className="rounded bg-red-500 px-4 py-2 text-white"
+              >
+                Remove
+              </button>
+            </div>
+          </article>
+        ))}
+        <p>Total: ${array.reduce((acc: any, item: any) => acc + item.item.price * item.quantity, 0)}</p>
+      </div>
+    </section>
+  );
+};
 
 export default CartItems;
