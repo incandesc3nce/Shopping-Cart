f00f59890889a421d2c34ce0e5d6c6fac92afc48
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 18:51:15 +0700

    feat: improve responsiveness

--- shopping-cart/src/components/CartItems/CartItems.tsx
+++ shopping-cart/src/components/CartItems/CartItems.tsx
@@ -21,18 +21,18 @@ const CartItems = () => {
   return (
     <section>
       <h2 className="mt-4 text-center text-3xl font-semibold">Cart Items</h2>
-      <div className="m-2 flex flex-col gap-4">
+      <div className="max-w-100% m-2 flex flex-col items-center gap-4">
         {array.map((arrayItem: any) => (
-          <article className="min-h-30 flex items-center justify-between rounded-3xl bg-slate-200 p-1 shadow-md">
+          <article className="min-h-30 flex w-3/4 flex-col items-center justify-between rounded-3xl bg-slate-200 p-1 shadow-md md:flex-row md:p-4 pb-4">
             <img
               src={arrayItem.item.imgUrl}
               alt={arrayItem.item.title}
-              className="h-20 w-20 rounded-full object-cover"
+              className="h-28 w-28 rounded-full object-cover"
             />
-            <h3 className="max-w-24 text-center text-xl font-semibold">
+            <h3 className="text-center text-xl font-semibold">
               {arrayItem.item.title}
             </h3>
-            <div>
+            <div className='flex flex-col items-center mb-4 mt-4'>
               <p className="text-xl">
                 ${arrayItem.item.price * arrayItem.quantity}
               </p>
@@ -51,7 +51,13 @@ const CartItems = () => {
             </div>
           </article>
         ))}
-        <p>Total: ${array.reduce((acc: any, item: any) => acc + item.item.price * item.quantity, 0)}</p>
+        <p>
+          Total: $
+          {array.reduce(
+            (acc: any, item: any) => acc + item.item.price * item.quantity,
+            0,
+          )}
+        </p>
       </div>
     </section>
   );
--- shopping-cart/src/components/ShopItems/ShopItems.tsx
+++ shopping-cart/src/components/ShopItems/ShopItems.tsx
@@ -5,7 +5,7 @@ const ShopItems = () => (
   <section className="m-5">
     <h2 className="mt-4 text-center text-3xl font-semibold">Shop Items</h2>
     <div
-      className="mt-4 grid gap-4"
+      className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
     >
       {items.map((item) => (
         <ItemCard key={item.id} item={item} />
