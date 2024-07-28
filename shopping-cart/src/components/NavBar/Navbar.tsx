d1810ddb821c52fcff09224c9ad886dde9871293
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 13:22:47 +0700

    feat: create routing links in navbar

--- shopping-cart/src/components/NavBar/Navbar.tsx
+++ shopping-cart/src/components/NavBar/Navbar.tsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => (
-  <header className="pl-4 pr-4 flex space-x-10 p-2 font-sans bg-sky-200">
+  <header className="flex space-x-10 bg-sky-200 p-2 pl-4 pr-4 font-sans">
     <h1 className="text-3xl font-bold">KUWS</h1>
     <nav className="flex w-full items-center">
       <ul className="flex w-full list-none space-x-4">
-        <li>Home</li>
-        <li>Shop</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
       </ul>
-      <div>Cart</div>
+      <div>
+        <Link to="cart">Cart</Link>
+      </div>
     </nav>
   </header>
 );
