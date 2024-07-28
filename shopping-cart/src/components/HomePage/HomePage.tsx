e31d5e19b725dbc4ffc3238dc2e78e25c2aacf1e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 11:48:46 +0700

    feat: improve text position

--- shopping-cart/src/components/HomePage/HomePage.tsx
+++ shopping-cart/src/components/HomePage/HomePage.tsx
@@ -4,25 +4,25 @@ const HomePage = () => (
       <div className="flex justify-center">
         <img src="src/assets/home_kaws.jpg" alt="Kaws logo" />
       </div>
-      <div className="text-center text-3xl">
+      <div className="text-center text-3xl flex flex-col items-center">
         <p>Welcome to</p>
         <b>KUWS Exclusive</b>
         <p>The best online store for Kaws.</p>
+        <br />
+        <p className="ml-4 mr-4 max-w-2xl text-lg">
+          This is not a real store btw. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Commodi corporis consequuntur ipsum aperiam. Iusto,
+          voluptatem veritatis. Accusantium, cum quo nisi accusamus repellendus
+          modi nobis veniam distinctio ducimus impedit voluptatum vitae!
+          Perspiciatis fugit ut consectetur, sunt dolor quos! Obcaecati autem
+          doloribus quo quibusdam voluptates beatae at placeat officia molestiae
+          cumque debitis atque consequatur soluta ratione omnis reiciendis a,
+          nisi minus tenetur! Eligendi quidem inventore ratione, harum rerum
+          perferendis commodi error. Culpa, velit! Sapiente similique vero autem
+          asperiores earum perspiciatis, officia vitae odio ducimus minus cumque
+          eaque architecto illo expedita et nostrum.
+        </p>
       </div>
-      <br />
-      <p className="ml-4 mr-4 text-lg">
-        This is not a real store btw. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Commodi corporis consequuntur ipsum aperiam. Iusto,
-        voluptatem veritatis. Accusantium, cum quo nisi accusamus repellendus
-        modi nobis veniam distinctio ducimus impedit voluptatum vitae!
-        Perspiciatis fugit ut consectetur, sunt dolor quos! Obcaecati autem
-        doloribus quo quibusdam voluptates beatae at placeat officia molestiae
-        cumque debitis atque consequatur soluta ratione omnis reiciendis a, nisi
-        minus tenetur! Eligendi quidem inventore ratione, harum rerum
-        perferendis commodi error. Culpa, velit! Sapiente similique vero autem
-        asperiores earum perspiciatis, officia vitae odio ducimus minus cumque
-        eaque architecto illo expedita et nostrum.
-      </p>
     </main>
   </div>
 );
