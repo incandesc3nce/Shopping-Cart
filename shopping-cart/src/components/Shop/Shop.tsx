ef97a4918dcaa29e47375b2c6c654e5bd953419e
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-07-28 15:00:13 +0700

    feat: create Shop component and routing for it

---
+++ shopping-cart/src/components/Shop/Shop.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../NavBar/Navbar';
+import Footer from '../Footer/Footer';
+import ShopItems from '../ShopItems/ShopItems';
+
+const Shop = () => (
+  <>
+    <Navbar />
+    <main>
+      <ShopItems />
+    </main>
+    <Footer />
+  </>
+);
+
+export default Shop;
--- shopping-cart/src/routes/routes.tsx
+++ shopping-cart/src/routes/routes.tsx
@@ -1,11 +1,16 @@
 import App from '../App';
 import ErrorPage from '../components/ErrorPage/ErrorPage';
+import Shop from '../components/Shop/Shop';
 
 const routes = [
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: '/shop',
+    element: <Shop />,
   },
 ];
 
